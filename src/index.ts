09ee7107be03b80480d75ac74db35bc4a48d989b
penevl <penevl@protonmail.com>
2023-05-03 09:31:25 +0300

    Added basic logging functionality

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import Gamedig from 'gamedig'
 import logger from 'skinwalker'
+import nodeCron from 'node-cron'
+import mongoose from 'mongoose'
+import ServerState from '../models/serverState.js'
 import { readFileSync } from 'fs'
 
 interface Options {
@@ -7,9 +10,8 @@ interface Options {
     databaseIp: string,
     databaseUsername: string,
     databasePasswd: string,
-    queryTimes: number,
-    missionEndTimes: string[],
-    missionStartTimes: string[],
+    cronTimezone: string
+    missionTimes: string[],
     logLevel: "TRACE" | "INFO" | "WARN" | "ERROR"
 }
 
@@ -51,11 +53,65 @@ logger.init(getOptions().logLevel, {
     errorFileLocation: 'data/logs/',
 })
 
+logger.trace(`Options: ${JSON.stringify(getOptions())}`, `query`)
+
+getOptions().missionTimes.forEach((time: string) => {
+
+    logger.info(`Setting up new cron job on time ${time}`, `query`)
+
+    nodeCron.schedule(time,() => {
+
+        logger.info(`Attempting to log server state to database`, `query`)
+
+        getServerData().then(async (response: ServerResponse) => {
+
+            logger.info(`Got server data`, `query`)
+            logger.trace(`serverResponse: ${JSON.stringify(response)}`)
+
+            const serverState = new ServerState(response)
+            await serverState.save().then(() => {
+                logger.info(`Server state successfully saved to database`, `query`)
+            }).catch(err => {
+                logger.error(`Failed to save server state to database`, `query`)
+                logger.error(err.message, `query`)
+            })
+
+        }).catch(err => {
+            logger.erro(`Encountered error quering server`, `query`)
+            logger.error(err.message, `query`)
+        })
+
+    },{
+        scheduled: true,
+        timezone: getOptions().cronTimezone
+    })
+
+})
+
+connectToDatabase()
+
+async function connectToDatabase() {
+
+    const uname = getOptions().databaseUsername
+    const passwd = getOptions().databasePasswd
+    const ip = getOptions().databaseIp
+    const final = `mongodb+srv://${uname}:${passwd}@${ip}`
+
+    logger.info(`Attempting to conncet to database`, `query`)
+    await mongoose.connect(final).then(() => {
+        logger.info(`Successfuly connected to database`, `query`)
+    }).catch(err => {
+        logger.error(`Failed to connect to database`, `query`)
+        logger.error(err.message, `query`)
+    })
+
+}
+
 function getOptions(): Options {
     return JSON.parse(readFileSync('data/query-options.json', { encoding: 'utf8', flag: 'r' }));
 }
 
-function getServerData(){
+function getServerData() {
 
     var toReturn = Gamedig.query({
         type: 'arma3',
